c001c1076bbcc37de7f274968ec8cd9d82b18d09
josephgalloway321 <josephgalloway520@gmail.com>
2023-01-20 22:53:16 -0700

    Added section in installation searching for firmware folder in boot

--- src/install.sh
+++ src/install.sh
@@ -3,31 +3,45 @@
 # Update everything
 sudo apt update && sudo apt upgrade
 
+# Ubuntu Server 22.04 LTS has the config.txt file in /boot/firmware
+# Raspberry Pi OS has the config.txt file in /boot
+# So... check if there's a firmware folder or not first
 
-# Make changes to the config.txt file so the red light turns on during startup
-# First, create a backup for config.txt and name it config_backup.txt
-sudo cp /boot/firmware/config.txt /boot/firmware/config_backup.txt
+if [ -d "/boot/firmware" ]; then
+    # For Ubuntu 22.04 LTS installation
+    # Make changes to the config.txt file so the red light turns on during startup
+    # First, create a backup for config.txt and name it config_backup.txt
+    sudo cp /boot/firmware/config.txt /boot/firmware/config_backup.txt
 
-# Append to the config.txt to make red pin output low during startup
-echo "gpio=4=op,dl" | sudo tee --append /boot/firmware/config.txt
+    # Append to the config.txt to make red pin output low during startup
+    echo "gpio=4=op,dl" | sudo tee --append /boot/firmware/config.txt
+else 
+    # For Raspberry Pi OS installation
+    # Make changes to the config.txt file so the red light turns on during startup
+    # First, create a backup for config.txt and name it config_backup.txt
+    sudo cp /boot/config.txt /boot/config_backup.txt
+    
+    # Append to the config.txt to make red pin output low during startup
+    echo "gpio=4=op,dl" | sudo tee --append /boot/config.txt
+fi
 
 
 # Make sure the required libraries are installed
 # Check if Python3 is installed
 if [ $(command -v python3) ]; then
-  echo "Python3 is already installed."
+    echo "Python3 is already installed."
 else
-  echo "Python 3 is not installed. Installing now..."
-  sudo apt-get install python3
+    echo "Python 3 is not installed. Installing now..."
+    sudo apt-get install python3
 fi
 
 # Check if python3-gpiozero is installed
 PACKAGE_INSTALLED=$(dpkg-query -W --showformat='${Status}\n' "python3-gpiozero")
 if [ "$PACKAGE_INSTALLED" == "install ok installed" ]; then
-  echo "GPIO Zero is already installed."
+    echo "GPIO Zero is already installed."
 else
-  echo "GPIO Zero is not installed. Installing now..."
-  sudo apt-get install python3-gpiozero
+    echo "GPIO Zero is not installed. Installing now..."
+    sudo apt-get install python3-gpiozero
 fi
 
 # Install Python and service scripts then place them in the correct folders
